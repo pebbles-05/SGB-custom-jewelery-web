6b07bbf10c3fbb10b8b7aa9ef94ed8a195a0fe9f
Ari220600 <arindamchakraborty2k@gmail.com>
2024-11-08 19:08:36 +0530

    animation on name

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,18 +1,52 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import "./Hero.css";
 
 const Hero = () => {
   return (
     <div className="w-full h-[80vh] overflow-hidden relative flex items-center text-custom-bg-light">
       <div className="text-5xl flex flex-col ml-16 gap-8 justify-start items-start">
-        <span>শ্রেয়সীর গয়নার বাক্স</span>
+      <span className="shiny-text">
+        <span className="shiny-letter" style={{ animationDelay: `${0 * 0.3}s` }}>
+        শ্রে
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${1 * 0.3}s` }}>
+        য়
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${2 * 0.3}s` }}>
+        সী
+        </span>
+        <span className="shiny-letter mr-5" style={{ animationDelay: `${3 * 0.3}s` }}>
+        র 
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${4 * 0.3}s` }}>
+        গ
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${5 * 0.3}s` }}>
+        য়
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${6 * 0.3}s` }}>
+        না
+        </span>
+        <span className="shiny-letter mr-5" style={{ animationDelay: `${7 * 0.3}s` }}>
+        র 
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${8 * 0.3}s` }}>
+        বা
+        </span>
+        <span className="shiny-letter" style={{ animationDelay: `${9 * 0.3}s` }}>
+        ক্স
+        </span>
+        </span>
+        {/* <span >শ্রেয়সীর গয়নার বাক্স</span> */}
+        {/* <span className="Hero-name">শ্রেয়সীর গয়নার বাক্স</span> */}
         <span className="text-3xl text-custom-bg-light/60">
           "create your own"
         </span>
         <Link
           href="/customize"
-          className="text-2xl border border-custom-bg-light px-8 py-4 rounded"
+          className="text-2xl border border-custom-bg-light px-8 py-4 rounded hover:bg-custom-bg-light hover:text-custom-black"
         >
           Customize
         </Link>
