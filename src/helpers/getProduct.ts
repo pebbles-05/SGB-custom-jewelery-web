5296b330773bd24ba7d285d08374c6dd0f6e6807
Ari220600 <arindamchakraborty2k@gmail.com>
2024-11-25 01:18:04 +0530

    helpers updated

--- src/helpers/getProduct.ts
+++ src/helpers/getProduct.ts
@@ -1,8 +1,8 @@
 import type { Product } from "@/interface/interfaces";
-import producData from "@/enums/productData.json";
+import {productData} from "@/enums/productData.js";
 
 const getProduct = (id: string | number): Product | undefined => {
-  return producData.find((product) => product.id === id);
+  return productData.find((product) => product.id === id);
 };
 
 export { getProduct };
